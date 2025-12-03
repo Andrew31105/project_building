c0b08e026667647306f01781e193808b6f997f13
Andrew31105 <daoanh3112005ae@gmail.com>
2025-12-03 12:26:07 +0700

    Update project

--- Exercise-4/src/main/java/com/javaweb/api/web/NewAPI.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.api.web;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController(value = "newAPIOfWeb")
-public class NewAPI {
-}
